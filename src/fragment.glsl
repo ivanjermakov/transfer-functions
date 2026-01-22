c02add57bb99e109cb45d10ae61945872d939e0e
ivanjermakov <ivanjermakov1@gmail.com>
2026-01-22 12:04:09 +0100

    Raw transfer function

---
+++ src/fragment.glsl
@@ -0,0 +1,42 @@
+precision mediump float;
+
+varying vec2 v_uv;
+
+float hueToRgb(float p, float q, float t) {
+    if (t < 0.) t += 1.;
+    if (t > 1.) t -= 1.;
+
+    if (t < 1. /6.) return p + (q - p) * 6. * t;
+    if (t < 1. /2.) return q;
+    if (t < 2. /3.) return p + (q - p) * (2. / 3. - t) * 6.;
+
+    return p;
+}
+
+vec3 hslToRgb(vec3 hsl) {
+    float h = hsl.x;
+    float s = hsl.y;
+    float l = hsl.z;
+
+    float q = l < 0.5 ? l * (1.0 + s) : l + s - l * s;
+    float p = 2.0 * l - q;
+
+    return vec3(
+        hueToRgb(p, q, h + 1.0/3.0),
+        hueToRgb(p, q, h),
+        hueToRgb(p, q, h - 1.0/3.0)
+    );
+}
+
+vec3 tfRaw(vec3 x) {
+    return x;
+}
+
+void main() {
+    float hue = 1. - v_uv.y;
+    vec3 color = hslToRgb(vec3(hue, 1., .5));
+    float intensity = v_uv.x * 2.;
+    vec3 light = color * intensity;
+    vec3 mapped = tfRaw(light);
+    gl_FragColor = vec4(mapped, 1.);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,67 @@
 /* @refresh reload */
-import { Component } from 'solid-js'
+import { Component, onMount } from 'solid-js'
 import { render } from 'solid-js/web'
+import fragmentGlsl from './fragment.glsl?raw'
 import './index.css'
+import vertexGlsl from './vertex.glsl?raw'
+
+let canvas!: HTMLCanvasElement
+let gl!: WebGL2RenderingContext
 
 export const Main: Component = () => {
-    return <p>Hello</p>
+    onMount(() => {
+        gl = canvas.getContext('webgl2', { antialiasing: false })! as WebGL2RenderingContext
+        canvas.width = 1280
+        canvas.height = 720
+        const dpr = window.devicePixelRatio
+        canvas.style.width = `${canvas.width / dpr}px`
+        canvas.style.height = `${canvas.height / dpr}px`
+
+        const vertexShader = createShader(gl.VERTEX_SHADER, vertexGlsl)
+        const fragmentShader = createShader(gl.FRAGMENT_SHADER, fragmentGlsl)
+
+        const program = gl.createProgram()
+        gl.attachShader(program, vertexShader)
+        gl.attachShader(program, fragmentShader)
+        gl.linkProgram(program)
+
+        const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
+        const positionBuffer = gl.createBuffer()
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+
+        const positions = [-1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0]
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+
+        gl.viewport(0, 0, canvas.width, canvas.height)
+        gl.clearColor(0, 0, 0, 1)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+
+        gl.useProgram(program)
+        gl.enableVertexAttribArray(positionAttributeLocation)
+        gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0)
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6)
+    })
+
+    const createShader = (type: number, source: string) => {
+        const shader = gl.createShader(type)!
+        gl.shaderSource(shader, source)
+        gl.compileShader(shader)
+
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            const error = gl.getShaderInfoLog(shader)!
+            gl.deleteShader(shader)
+            throw Error(error)
+        }
+
+        return shader
+    }
+
+    return (
+        <>
+            <canvas ref={canvas} />
+        </>
+    )
 }
 
 render(() => <Main />, document.getElementById('root')!)
